686977a8567971082143f34ceb0ca070fbfe4189
willemml <willem@leit.so>
2021-03-25 10:14:35 -0700

    build client from clientbuilder

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 [![crates.io](https://img.shields.io/crates/v/wicrs_client.svg)](https://crates.io/crates/wicrs_client)
 [![docs.rs](https://docs.rs/wicrs_client/badge.svg)](https://docs.rs/wicrs_client)
 
-
 A client for WICRS Server, built using `wicrs_api`.
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,32 @@
 use std::collections::HashMap;
 
-use wicrs_api::wicrs_server::{ID, hub::Hub, user::User};
+use wicrs_api::{
+    wicrs_server::{hub::Hub, user::User, ID},
+    ClientBuilder, Result,
+};
 
 pub struct Client {
-    http_client: wicrs_api::Client,
-    current_user: User,
-    known_servers: HashMap<ID, Hub>,
+    pub http_client: wicrs_api::Client,
+    pub current_user: User,
+    pub known_hubs: HashMap<ID, Hub>,
+    pub known_users: HashMap<ID, User>,
+}
+
+impl Client {
+    pub async fn from_builder(builder: ClientBuilder) -> Result<Self> {
+        let http_client = builder.build().await?;
+        let user = http_client.get_user().await?;
+        let mut known_hubs = HashMap::new();
+        for hub_id in user.in_hubs.clone() {
+            if let Ok(hub) = http_client.get_hub(&hub_id).await {
+                known_hubs.insert(hub_id, hub);
+            }
+        }
+        Ok(Self {
+            current_user: user,
+            http_client,
+            known_hubs,
+            known_users: HashMap::new(),
+        })
+    }
 }
