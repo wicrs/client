5014f66e9f9dec9ecb6b4b0bee919152804a9625
willemml <willem@leit.so>
2021-03-24 21:56:42 -0700

    start on client lib

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # WICRS Client
+
 [![crates.io](https://img.shields.io/crates/v/wicrs_client.svg)](https://crates.io/crates/wicrs_client)
 [![docs.rs](https://docs.rs/wicrs_client/badge.svg)](https://docs.rs/wicrs_client)
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+use wicrs_api::wicrs_server::{ID, hub::Hub, user::User};
+
+pub struct Client {
+    http_client: wicrs_api::Client,
+    current_user: User,
+    known_servers: HashMap<ID, Hub>,
 }
